765d8e74198c6aade7cec4dc973375a6ea3113c4
sachin93094 <sachin93094@gmail.com>
2024-05-09 06:29:23 +0000

    updated o/p files

--- CMakeFiles/runTests.dir/cmake_clean.cmake
+++ CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -1,8 +1,8 @@
 file(REMOVE_RECURSE
   "CMakeFiles/runTests.dir/tests.cpp.o"
   "CMakeFiles/runTests.dir/tests.cpp.o.d"
-  "bin/runTests"
-  "bin/runTests.pdb"
+  "runTests"
+  "runTests.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
